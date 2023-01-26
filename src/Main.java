6de50b300d7da8d95bb6d5805ed877ab1736caca
jcanaviri42 <jcanaviri42@gmail.com>
2023-01-26 00:02:49 -0400

    variables and use of scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Declaring an integer variable
+        int myIntegerVariable = 42;
+        System.out.println(myIntegerVariable);
+
+        // Modifying a variable
+        myIntegerVariable = 10;
+        System.out.println(myIntegerVariable);
+
+        // Declaring a string
+        String myStringVariable = "Greetings";
+        System.out.println(myStringVariable);
+
+        myStringVariable = "Bolivia";
+        System.out.println(myStringVariable);
+
+        // How to know the type of variable
+        String variableType = myStringVariable.getClass().getSimpleName();
+        System.out.println("The type of myStringVariable is: " + variableType);
+
+        // var - resolves the type of the variable itself
+        var myInt = 10;
+        System.out.println(myInt);
+        System.out.println("myInt = " + myInt);
+
+        // Concatenation
+        var user = "Josue";
+        var degree = "Engineer";
+
+        var union = degree + " " + user;
+        System.out.println(union);
+
+        // Special characters
+        System.out.println("Your name is: \n" + user);
+        System.out.println("Your name is: \t" + user);
+        System.out.println("Your name is: \b\b" + user);
+
+        // Scanner
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please write your name: ");
+        user = sc.nextLine();
+        System.out.println("So you are the user: " + user);
+
+        sc.close();
     }
 }
