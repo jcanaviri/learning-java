53a90dada1c3e5adcce6005f7c715fd320e6a1e0
jcanaviri42 <jcanaviri42@gmail.com>
2023-02-11 23:16:22 -0400

    Update override in java

--- src/overrriting/Main.java
+++ src/overrriting/Main.java
@@ -4,6 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Manager german = new Manager("German", 5000, "Software");
 
+        System.out.println(german.getSalary());
+        german.setSalary(7000);
+        System.out.println(german.getSalary());
+
+        german.setDepartment("New Department");
+        System.out.println(german.getDepartment());
+
         System.out.println(german.getDetails());
     }
 }
