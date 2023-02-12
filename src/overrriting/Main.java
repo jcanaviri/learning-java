73957e6a4883c91477fb3ba215f15593bb586eae
jcanaviri42 <jcanaviri42@gmail.com>
2023-02-11 23:13:56 -0400

    Override in java

---
+++ src/overrriting/Main.java
@@ -0,0 +1,9 @@
+package overrriting;
+
+public class Main {
+    public static void main(String[] args) {
+        Manager german = new Manager("German", 5000, "Software");
+
+        System.out.println(german.getDetails());
+    }
+}
