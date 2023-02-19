bee6cb3ede54ec91a198b2d210391ab174554f15
jcanaviri42 <jcanaviri42@gmail.com>
2023-02-19 16:29:45 -0400

    Finish the service module

---
+++ src/movies/com/example/services/IMovies.java
@@ -0,0 +1,14 @@
+package movies.com.example.services;
+
+@SuppressWarnings("unused")
+public interface IMovies {
+    String FILENAME = "movies.txt";
+
+    void addMovie(String newMovie);
+
+    void getAllMovies();
+
+    void searchMovie(String movieName);
+
+    void startMovieDatabase();
+}
---
+++ src/movies/com/example/services/MoviesService.java
@@ -0,0 +1,62 @@
+package movies.com.example.services;
+
+import movies.com.example.data.DataAccessImp;
+import movies.com.example.data.IDataAccess;
+import movies.com.example.domain.Movie;
+import movies.com.example.exceptions.DataAccessEx;
+import movies.com.example.exceptions.ReadDataEx;
+
+import java.util.List;
+
+@SuppressWarnings("unused")
+public class MoviesService implements IMovies {
+
+    private final IDataAccess db;
+
+    public MoviesService() {
+        this.db = new DataAccessImp();
+    }
+
+    @Override
+    public void addMovie(String newMovie) {
+        Movie movie = new Movie(newMovie);
+
+        try {
+            if (this.db.isAlreadyRegistered(FILENAME))
+                this.db.write(movie, FILENAME, false);
+        } catch (DataAccessEx e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void getAllMovies() {
+        try {
+            List<Movie> allMovies = this.db.getAll(FILENAME);
+            allMovies.forEach(System.out::println);
+        } catch (ReadDataEx e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void searchMovie(String movieName) {
+        try {
+            System.out.println(this.db.search(FILENAME, movieName));
+        } catch (ReadDataEx e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void startMovieDatabase() {
+        try {
+            if (this.db.isAlreadyRegistered(FILENAME))
+                this.db.delete(FILENAME);
+
+            this.db.insert(FILENAME);
+        } catch (DataAccessEx e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
