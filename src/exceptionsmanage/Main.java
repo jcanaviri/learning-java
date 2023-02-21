1d361aa6fed2de466705bb6855bebab42329315d
jcanaviri42 <jcanaviri42@gmail.com>
2023-02-20 20:02:50 -0400

    Add more examples to the exceptions

--- src/exceptionsmanage/Main.java
+++ src/exceptionsmanage/Main.java
@@ -4,15 +4,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        // There are many exceptions in java
+        // these are the more popular
+        try {
+            int a = 10, b = 0;
 
-        System.out.print("Give me a number: ");
-        int number = sc.nextInt();
-        int result;
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
 
-        result = Arithmetic.division(42, number);
+        try {
+            int a = Integer.parseInt("Josue");
+
+            System.out.println(a);
+        } catch (NumberFormatException e) {
+            System.out.println("Cannot format a string to a number");
+        }
+
+        try {
+            int[] a = new int[3];
+            a[5] = 42;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Out of the range");
+        }
+
+        int result = Arithmetic.division(42, 0);
         System.out.println(result);
-
-        sc.close();
     }
 }
