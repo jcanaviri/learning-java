6f6f89878062a9ef732e69ab164acff48e4fb8e6
jcanaviri42 <jcanaviri42@gmail.com>
2023-02-22 20:50:05 -0400

    Update exceptions

--- src/exceptionsmanage/Main.java
+++ src/exceptionsmanage/Main.java
@@ -1,13 +1,15 @@
 package exceptionsmanage;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         // There are many exceptions in java
         // these are the more popular
         try {
-            int a = 10, b = 0;
-
-            System.out.println(a / b);
+            // * This code throws the error of zero division
+            // System.out.println(15 / 0);
+            System.out.println(15 / 3);
         } catch (ArithmeticException e) {
             System.out.println("Cannot divide by zero");
         }
@@ -22,7 +24,14 @@ public class Main {
 
         try {
             int[] a = new int[3];
-            a[5] = 42;
+
+            a[2] = 42;
+
+            // * This will throw an error because we are trying to access
+            // * the fifth element of an array with just three
+            // a[5] = 42;
+
+            System.out.println("a = " + Arrays.toString(a));
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Out of the range");
         }
