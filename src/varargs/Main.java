137cd6d6e63689864ca8b16ac5f71ad3e1b9d168
jcanaviri42 <jcanaviri42@gmail.com>
2023-04-20 23:19:52 -0400

    Variables arguments

--- src/varargs/Main.java
+++ src/varargs/Main.java
@@ -4,20 +4,24 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("\n1-4: ");
         showNumbers(1, 2, 3, 4);
+
+        System.out.println("\n17-22: ");
         showNumbers(17, 22);
 
         manyParams("Josue", 5, 4, 3);
-        manyParams("Samanta", 3, 2, 1);
+        manyParams("Sarah", 3, 2, 1, 0);
     }
 
     private static void manyParams(String name, int... numbers) {
-        System.out.println("name = " + name);
+        System.out.println("\nname = " + name);
         showNumbers(numbers);
     }
 
+    // Prints all the given numbers
     private static void showNumbers(int... numbers) {
         Arrays.stream(numbers)
-                .forEach(System.out::println);
+                .forEach(System.out::print);
     }
 }
